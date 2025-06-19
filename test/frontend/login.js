e7911ef69d415391d95acee7b7844b3cb3ff48af
ftourangeau <francis.tourangeau@hrsdc-rhdcc.gc.ca>
2025-06-19 09:37:02 -0400

    30 days token tests + signature fix

--- test/frontend/login.js
+++ test/frontend/login.js
@@ -124,17 +124,21 @@ describe('login process', function () {
     });
 
     it('should be able to logout and render session invalid', function (browser) {
-      browser.click('.profile-dropbtn');
-      browser.keys(browser.Keys.ENTER);
-      browser.click('.navigation-user-logout');
-      browser.keys(browser.Keys.ENTER);
-      browser.assert.urlContains('#user/login');
-      browser.navigateTo(`http://localhost:${config.serverPort}/#dashboard`);
-      browser.expect.element('.swal2-html-container').text.to.equal('Your session has expired, click OK to log on again');
+      browser.perform(() => {
+        browser.click('.profile-dropbtn');
+        browser.keys(browser.Keys.ENTER);
+        browser.click('.navigation-user-logout');
+        browser.keys(browser.Keys.ENTER);
+        browser.assert.urlContains('#user/login');
+        browser.navigateTo(`http://localhost:${config.serverPort}/#dashboard`);
+        browser.pause(500);
+        browser.expect.element('.swal2-html-container').text.to.equal('Your session has expired, click OK to log on again');
+      });
     });
 
     it('should reject login from the old password', function (browser) {
       browser.navigateTo(`http://localhost:${config.serverPort}`);
+      browser.pause(1000);
       browser.assert.elementPresent('#login-input-username');
       browser.sendKeys('#login-input-username', testData.testUser.email);
       browser.assert.elementPresent('#login-input-password');
@@ -212,10 +216,159 @@ describe('login process', function () {
         });
       }
       browser.expect.element('#loginErrorMessage').text.to.equal('You have exceeded the maximum number of attempts to enter your one-time password. For your security, please reset your password by selecting the "Forgot Password?" option.');
+      browser.perform(() => {
+        database.collection("users").updateOne({ email: testData.testUser.email }, { $set: { failedMfaCount: 0 } }, function (err, commandResult) {
+          if (err) {
+            browser.assert.fail("Failed to reset count " + err.message);
+          }
+          browser.assert.equal(commandResult.result.nModified, 1);
+        });
+      });
+    });
+
+    var storedCookie;
+
+    it('should be able to remember mfa token for 30 days', function (browser) {
+      browser.navigateTo(`http://localhost:${config.serverPort}`);
+      browser.pause(500);
+      browser.assert.urlContains('#user/login');
+      browser.assert.elementPresent('#login-input-username');
+      browser.sendKeys('#login-input-username', testData.testUser.email);
+      browser.assert.elementPresent('#login-input-password');
+      browser.sendKeys('#login-input-password', [testData.testUser.thirdpassword, browser.Keys.ENTER]);
+      browser.assert.urlContains('#user/loginMfa');
+      var devEnv = config.devEnv;
+      var cookieName = devEnv ? `connect-${devEnv}.sid` : `connect.sid`;
+      browser.getCookie(cookieName, function callback(result) {
+        this.assert.equal(result.name, cookieName);
+        storedCookie = result;
+        var sessionID = result.value.split('.')[0].substring(4);
+        var validationTokenId;
+        database.collection("mfatokens").findOne({ sessionId: sessionID, verified: false }, function (err, result) {
+          if (err) {
+            browser.assert.fail("Failed to query validation token: " + err.message);
+          }
+          if (result && result.validationToken) {
+            validationTokenId = result.validationToken;
+          }
+          browser.assert.elementPresent('#skip-mfa');
+          browser.element('#skip-mfa').check();
+          browser.assert.elementPresent('#login-mfa-input-verificationcode');
+          browser.sendKeys('#login-mfa-input-verificationcode', [validationTokenId, browser.Keys.ENTER]);
+        });
+      });
+      browser.assert.urlContains('#dashboard');
+      var cookieName2 = devEnv ? `connect-${devEnv}.fid` : `connect.fid`;
+      browser.getCookie(cookieName2, function callback(result) {
+        this.assert.equal(result.name, cookieName2);
+      });
+      browser.click('.profile-dropbtn');
+      browser.keys(browser.Keys.ENTER);
+      browser.click('.navigation-user-logout');
+      browser.keys(browser.Keys.ENTER);
+    });
+
+    it('should accept login with stored mfa cookie', function (browser) {
+      browser.navigateTo(`http://localhost:${config.serverPort}`);
+      browser.pause(500);
+      browser.assert.urlContains('#user/login');
+      browser.assert.elementPresent('#login-input-username');
+      browser.sendKeys('#login-input-username', testData.testUser.email);
+      browser.assert.elementPresent('#login-input-password');
+      browser.sendKeys('#login-input-password', [testData.testUser.thirdpassword, browser.Keys.ENTER]);
+      browser.pause(2000);
+      browser.assert.urlContains('#dashboard');
+      browser.perform(() => {
+        browser.assert.elementPresent('.profile-dropbtn');
+        browser.click('.profile-dropbtn');
+        browser.keys(browser.Keys.ENTER);
+        browser.click('.navigation-user-logout');
+        browser.keys(browser.Keys.ENTER);
+      });
+    });
+
+    it('should reject login with stored mfa cookie that is expired', function (browser) {
+      var devEnv = config.devEnv;
+      var cookieName = devEnv ? `connect-${devEnv}.fid` : `connect.fid`;
+      browser.getCookie(cookieName, function callback(result) {
+        this.assert.equal(result.name, cookieName);
+        var tokenID = result.value.split('.')[0].substring(6);
+        const fortyFiveDaysAgo = new Date();
+        fortyFiveDaysAgo.setDate(fortyFiveDaysAgo.getDate() - 45);
+        database.collection("mfatokens").updateOne(
+          { tokenId: tokenID, verified: true }, // filter
+          { $set: { validationDate: fortyFiveDaysAgo } }, // update
+          function (err, result) {
+            if (err) {
+              browser.assert.fail("Failed to update validation date: " + err.message);
+              return;
+            }
+            browser.navigateTo(`http://localhost:${config.serverPort}`);
+            browser.pause(500);
+            browser.assert.urlContains('#user/login');
+            browser.assert.elementPresent('#login-input-username');
+            browser.sendKeys('#login-input-username', testData.testUser.email);
+            browser.assert.elementPresent('#login-input-password');
+            browser.sendKeys('#login-input-password', [testData.testUser.thirdpassword, browser.Keys.ENTER]);
+            browser.pause(2000);
+            browser.assert.urlContains('#user/loginMfa');
+            database.collection("mfatokens").updateOne(
+              { tokenId: tokenID, verified: true }, // filter
+              { $set: { validationDate: new Date() } }, // update
+              function (err, result) {
+                if (err) {
+                  browser.assert.fail("Failed to update validation date: " + err.message);
+                  return;
+                }
+              }
+            );
+          }
+        );
+      });
+    });
+
+    it('should reject login with stored mfa if tokenId is wrong', function (browser) {
+      browser.navigateTo(`http://localhost:${config.serverPort}/#user/login`);
+      browser.pause(2500);
+      browser.assert.urlContains('#user/login');
+      var devEnv = config.devEnv;
+      var cookieName2 = devEnv ? `connect-${devEnv}.fid` : `connect.fid`;
+      browser.getCookie(cookieName2, function callback(result) {
+        var cookie = result;
+        browser.cookies.set({
+          name: cookie.name,
+          value: 's%253A5mn7X5wTyDT45hTv8-m3lHFUvvo_dyY4.aIRqsk9F1Hv6dEa061usdSuV9jDVfWFm7AFj4cqygyg',
+          path: '/'
+        })
+        browser.assert.elementPresent('#login-input-username');
+        browser.sendKeys('#login-input-username', testData.testUser.email);
+        browser.assert.elementPresent('#login-input-password');
+        browser.sendKeys('#login-input-password', [testData.testUser.thirdpassword, browser.Keys.ENTER]);
+        browser.assert.urlContains('#user/loginMfa');
+      });
+    });
+
+    it('should reject login with stored mfa if tokenId signature is wrong', function (browser) {
+      browser.navigateTo(`http://localhost:${config.serverPort}`);
+      browser.pause(500);
+      browser.assert.urlContains('#user/login');
+      var cookieValue = storedCookie.value.slice(0, -1) + "?";
+      browser.cookies.set({
+        name: storedCookie.name,
+        value: cookieValue,
+        path: '/'
+      })
+      browser.assert.elementPresent('#login-input-username');
+      browser.sendKeys('#login-input-username', testData.testUser.email);
+      browser.assert.elementPresent('#login-input-password');
+      browser.sendKeys('#login-input-password', [testData.testUser.thirdpassword, browser.Keys.ENTER]);
+      browser.assert.urlContains('#user/loginMfa');
     });
 
   });
+
   after(function (browser) {
     browser.end();
   });
+
 });
