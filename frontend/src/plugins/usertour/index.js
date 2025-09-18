c1412fa8dd895001c7bc18ffa145e137bfeaeb73
ftourangeau <francis.tourangeau@hrsdc-rhdcc.gc.ca>
2025-09-18 09:05:50 -0400

    switched to local sdk

--- frontend/src/plugins/usertour/index.js
+++ frontend/src/plugins/usertour/index.js
@@ -4,7 +4,10 @@ define(function (require) {
   Origin.on('origin:dataReady login:changed', function () {
     if (Origin.constants['useUsertour']) {
       window.USERTOURJS_ENV_VARS = {
-        WS_URI: Origin.constants['usertourWS']
+        WS_URI: Origin.constants['usertourWS'],
+        ASSETS_URI: Origin.constants['usertourWS'] + "/sdk/",
+        USERTOURJS_ES2020_URL: Origin.constants['usertourWS'] + '/sdk/es2020/usertour.js',
+        USERTOURJS_LEGACY_URL: Origin.constants['usertourWS'] + "/sdk/legacy/usertour.iife.js"
       };
       usertour.init(Origin.constants['usertourToken']);
       usertour.identifyAnonymous();
