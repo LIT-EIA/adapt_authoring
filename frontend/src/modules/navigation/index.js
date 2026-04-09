fe832812a865e5ec91d61bcc58f134862dc35d23
Allouise Sibanda <211011505+Alloe8@users.noreply.github.com>
2026-04-09 15:28:06 -0600

    so many changes pt 11

--- frontend/src/modules/navigation/index.js
+++
@@ -1,5 +0,0 @@
-define(['core/origin', './views/navigationView'], function(Origin, NavigationView) {
-  Origin.once('origin:dataReady', function() {
-    $('#navigation_container').replaceWith(new NavigationView({ model: Origin.sessionModel }).$el);
-  });
-});
--- index.js
+++
@@ -1,7 +0,0 @@
-// LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
-var app = require('./lib/application');
-if (process.env.NODE_ENV !== 'test') {
-  require('dotenv').config();
-}
-
-module.exports = app;
