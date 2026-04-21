1187446301f28690e3b381c84afb85c337839ab7
Allouise Sibanda <211011505+Alloe8@users.noreply.github.com>
2026-04-21 09:39:33 -0600

    migrate . sh

--- docker/migration/migrate.sh
+++ docker/migration/migrate.sh
@@ -13,12 +13,12 @@ echo "📦 Restoring MongoDB dump..."
 mongorestore --host "${MONGO_HOST:-mongo}" --port 27017 /migration/dump
 
 echo "📁 Seeding assets..."
-mkdir -p /app/storage/data
-cp -r /migration/data/* /app/storage/data/
+mkdir -p /app/data
+cp -r /migration/data/* /app/data/
 
 echo "📁 Seeding framework..."
-mkdir -p /app/storage/framework
-cp -r /migration/framework/${TENANT_ID} /app/storage/framework/${TENANT_ID}
+#mkdir -p /app/temp/framework
+cp -r /migration/framework/${TENANT_ID} /app/temp/
 
 echo "📁 Preparing plugin type directories..."
 mkdir -p /app/storage/plugins/{content,component,extension,theme,output,auth}
